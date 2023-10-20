e9d0f128728199e7b0851bb0e842b16fa05d5cba
trilm <tri.le@floatingcube.com>
2023-10-20 15:26:52 +0700

    update code

--- TestToolLogin/MainWindow.xaml.cs
+++ TestToolLogin/MainWindow.xaml.cs
@@ -3,9 +3,11 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Xml.Linq;
 
 namespace TestToolLogin
@@ -22,62 +24,75 @@ namespace TestToolLogin
         {
             InitializeComponent();
 
-            users = new List<User>
+
+            users = CsvHelper.ReadUserCSV();
+            if (!users.Any())
             {
-                new User { Id = 1, Name = "Phúc Thiên (Test)", Url = "https://www.facebook.com/messages/t/100013640581333" },
-                //new User { Id = 2, Name = "Bao Ngoc (Test)", Url = "https://www.facebook.com/messages/t/100071894229494" },
-                //new User { Id = 3, Name = "Tri Le (Test)", Url = "https://www.facebook.com/messages/t/100004393085762" },
-            };
-
+                users = new List<User>
+                        {
+                            new User { Name = "Phúc Thiên (Test)", Url = "https://www.facebook.com/messages/t/100013640581333" },
+                        };
+            }
             dataGrid.ItemsSource = users;
-
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--disable-notifications");
-
             driver = new ChromeDriver(options);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
 
             if (string.IsNullOrEmpty(userName.Text) || string.IsNullOrEmpty(passWord.Password))
                 return;
             try
             {
-                Task.Run(() =>
-                {
-                    driver.Navigate().GoToUrl("https://www.facebook.com/");
-                    Dispatcher.Invoke(async () =>
-                    {
-                        try
-                        {
-                            driver.FindElement(By.Id("email")).SendKeys(userName.Text);
-                            driver.FindElement(By.Id("pass")).SendKeys(passWord.Password);
-                            driver.FindElement(By.Name("login")).Click();
-
-                            int checkLogin = 0;
-                            while (checkLogin <= 5)
-                            {
-                                try
-                                {
-                                    var checkMessenger = driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div/div/div/div/div/label/input"));
-                                    break;
-                                }
-                                catch
-                                {
-                                    await Task.Delay(1000);
-                                    checkLogin++;
-                                }
-                            }
-
-                            isLoginSucsess = true;
-                            LockBorder.Visibility = Visibility.Collapsed;
-                        }
-                        catch { }
-                    });
-                });
+                await Task.Run(async () =>
+                 {
+                     await Login();
+                 });
+                LockUI(false);
             }
             catch { }
+        }
+
+        private async Task Login()
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    driver.Navigate().GoToUrl("https://www.facebook.com");
+                    Dispatcher.InvokeAsync(() =>
+                    {
+                        LockUI(true);
+                        driver.FindElement(By.Id("email")).SendKeys(userName.Text);
+                        driver.FindElement(By.Id("pass")).SendKeys(passWord.Password);
+                        driver.FindElement(By.Name("login")).Click();
+                    });
+                });
+
+                int checkLogin = 0;
+                while (checkLogin <= 5)
+                {
+                    try
+                    {
+                        var checkMessenger = driver.FindElement(By.XPath("/html/body/div[1]/div/div[1]/div/div[2]/div[3]/div/div/div/div/div/label/input"));
+                        break;
+                    }
+                    catch
+                    {
+                        await Task.Delay(1000);
+                        checkLogin++;
+                    }
+                }
+            }
+            catch { }
+        }
+        private async void LockUI(bool isLocked)
+        {
+            isLoginSucsess = !isLocked;
+            LockBorder.Visibility = isLocked ? Visibility.Visible : Visibility.Collapsed;
+            Indicator.Visibility = isLocked ? Visibility.Visible : Visibility.Collapsed;
         }
 
         private void btnSend_Click(object sender, RoutedEventArgs e)
@@ -90,6 +105,7 @@ namespace TestToolLogin
 
         private void Window_Closed(object sender, EventArgs e)
         {
+            CsvHelper.WriteUserCSV(users);
             driver.Quit();
         }
 
@@ -116,42 +132,58 @@ namespace TestToolLogin
             {
                 foreach (var user in users)
                 {
-                    Dispatcher.Invoke(() =>
+                    Dispatcher.InvokeAsync(async() =>
                     {
                         try
                         {
-                            SendMessageFuntion(user);
+                            await SendMessageFuntion(user);
                         }
                         catch
                         {
-                            SendMessageFuntion(user);
+                            await SendMessageFuntion(user);
                         }
                     });
                 }
             });
         }
 
-        private void SendMessageFuntion(User user)
+        private async Task SendMessageFuntion(User user)
         {
-            IWebElement element = null;
-            driver.Navigate().GoToUrl(user.Url);
-            do
+            try
             {
-                try
+                IWebElement element = null;
+                driver.Navigate().GoToUrl(user.Url);
+                do
                 {
-                    element = driver.FindElement(By.CssSelector(".notranslate"));
-                }
-                catch { }
+                    try
+                    {
+                        element = driver.FindElement(By.CssSelector(".notranslate"));
+                    }
+                    catch { }
 
+                }
+                while (element == null);
+                element.SendKeys($"{noiDungTb.Text} \r\n");
             }
-            while (element == null);
-            element.SendKeys($"{noiDungTb.Text} \r\n");
+            catch
+            {
+            }
+        }
+
+        private void Save_CSV_Click(object sender, RoutedEventArgs e)
+        {
+            CsvHelper.WriteUserCSV(users);
+        }
+
+        private void Load_CSV_Click(object sender, RoutedEventArgs e)
+        {
+            users = CsvHelper.ReadUserCSV();
+            dataGrid.ItemsSource = users;
         }
     }
 
     public class User
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
     }
